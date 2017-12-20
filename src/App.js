5dd297ef50fa3f5db3bad24abe5a65916438788f
jaketrent <trent.jake@gmail.com>
2017-12-20 16:39:08 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+// @flow
+
+import AppBar from 'material-ui/AppBar'
+import { Card, CardHeader, CardText } from 'material-ui/Card'
+import gravatarUrl from 'gravatar-url'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import React, { Component } from 'react'
+import './App.css'
+
+type Props = {}
+
+type Message = {
+  name: string,
+  email: string,
+  date: string,
+  text: string
+}
+
+type State = {
+  messages: Message[]
+}
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      messages: [
+        {
+          name: 'Jakeroo',
+          email: 'trent.jake@gmail.com',
+          date: '27 Dec 2017',
+          text: `
+            Key players will take ownership of their innovations by virtually
+            growing mobile stakeholders. We aim to globally virtualise our
+            architecture by ethically reusing our immersive end-to-end user
+            experiences.
+`
+        }
+      ]
+    }
+  }
+  componentDidMount() {
+    fetch('http://localhost:3004/api/v1/messages')
+      .then(res => {
+        if (res.status !== 200) {
+          console.log('response err', res)
+        }
+        return res.json()
+      })
+      .then(json => {
+        this.setState({ messages: json.data })
+      })
+      .catch(err => {
+        console.log('fetch err', err)
+      })
+  }
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <AppBar title="Robo Retro" showMenuIconButton={false} />
+          {this.state.messages.map((msg, i) => (
+            <Card expanded key={i}>
+              <CardHeader
+                title={msg.name}
+                subtitle={msg.date}
+                avatar={gravatarUrl(msg.email)}
+              />
+              <CardText>{msg.text}</CardText>
+            </Card>
+          ))}
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+export default App
