3b6c18597ff2c55ce7213c898549a879bb9f5b93
jaketrent <trent.jake@gmail.com>
2017-12-21 14:18:12 -0700

    request log and autoload env locally

--- server.go
+++ server.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/joho/godotenv/autoload"
+	"io"
+	"io/ioutil"
 	"jaketrent.com/roboretro/messages"
 	"log"
 	"net/http"
@@ -15,6 +20,22 @@ func hasDatabase(db *gorm.DB) gin.HandlerFunc {
 		c.Set("db", db)
 		c.Next()
 	}
+}
+
+func RequestLogger(c *gin.Context) {
+	buf, _ := ioutil.ReadAll(c.Request.Body)
+	rdr1 := ioutil.NopCloser((bytes.NewBuffer(buf)))
+	rdr2 := ioutil.NopCloser((bytes.NewBuffer(buf)))
+
+	fmt.Println(func(reader io.Reader) string {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(reader)
+		s := buf.String()
+		return s
+	}(rdr1))
+
+	c.Request.Body = rdr2
+	c.Next()
 }
 
 func main() {
@@ -30,6 +51,7 @@ func main() {
 
 	router := gin.Default()
 	router.Use(gin.Logger())
+	router.Use(RequestLogger)
 	router.Use(gin.Recovery())
 	router.Use(hasDatabase(db))
 	messages.Mount(router)
